87c452a60d8f5e962fad2656d556c55031758ca1
pipeosmu <pipe96-17@hotmail.com>
2019-09-20 15:38:07 -0500

    primer commit

---
+++ public/js/socket-chat.js
@@ -0,0 +1,57 @@
+var socket = io();
+
+var params = new URLSearchParams(window.location.search);
+
+if (!params.has('nombre') || !params.has('sala')) {
+    window.location = 'index.html';
+    throw new Error('el nombre y sala son obligatorios');
+}
+
+var usuario = {
+    nombre: params.get('nombre'),
+    sala: params.get('sala')
+}
+
+socket.on('connect', function() {
+    console.log('Conectado al servidor');
+    socket.emit('entrarChat', usuario, function(resp) {
+        console.log('Usuarios conectados ', resp);
+    });
+});
+
+// escuchar
+socket.on('disconnect', function() {
+
+    console.log('Perdimos conexión con el servidor');
+
+});
+
+
+// Enviar información
+// socket.emit('enviarMensaje', {
+//     usuario: 'Fernando',
+//     mensaje: 'Hola Mundo'
+// }, function(resp) {
+//     console.log('respuesta server: ', resp);
+// });
+
+// Escuchar información
+socket.on('crearMensaje', function(mensaje) {
+
+    console.log('Servidor:', mensaje);
+
+});
+
+//Escuchar cambios de usuarios / Entra o sale
+
+socket.on('listaPersona', function(personas) {
+
+    console.log(personas);
+
+});
+
+//mensajes privados
+
+socket.on('mensajePrivado', function(mensaje) {
+    console.log('Mensaje Privado: ', mensaje);
+});
